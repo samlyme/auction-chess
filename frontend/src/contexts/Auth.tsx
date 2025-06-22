bb6aa2e3208b3ae6352936ea39545d7b37591159
samlyme <sam@samly.me>
2025-06-21 21:44:41 -0700

    create auth context

---
+++ frontend/src/contexts/Auth.tsx
@@ -0,0 +1,46 @@
+import { createContext, useCallback, useContext, useState, type ReactNode } from "react";
+import type { UserCredentials } from "../schemas/types";
+import { usernamePasswordLogin } from "../services/auth";
+
+interface AuthContextType {
+    token: string | null;
+    login: (credentials: UserCredentials) => void;
+    logout: () => void;
+}
+
+const AuthContext = createContext<AuthContextType | null>(null);
+
+export function AuthProvider({ children }: { children: ReactNode}) {
+    const [token, setToken] = useState<string | null>(() => localStorage.getItem("access_token"))
+    
+    const login = useCallback( async (credentials: UserCredentials) => {
+        try {
+            const res = await usernamePasswordLogin(credentials);
+
+            localStorage.setItem("access_token", res.access_token)
+            setToken(res.access_token)
+        }
+        catch (err) {
+            console.error("failed to login", err);
+        }
+    }, [])
+
+    const logout = () => {
+        setToken(null);
+        localStorage.removeItem("access_token");
+    }
+
+    const context: AuthContextType = { token, login, logout }
+
+    return (
+        <AuthContext value={context}>
+            {children}
+        </AuthContext>
+    )
+}
+
+export function useAuthContext(): AuthContextType {
+    const out = useContext(AuthContext);
+    if (!out) throw Error("useAuthContext must be used within an AuthProvider")
+    return out;
+}
