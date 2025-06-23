2358237909fb9b799ceafab9a789e84e00d156a7
samlyme <sam@samly.me>
2025-06-22 22:36:01 -0700

    vite proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
     proxy: {
       '/api': {
         target: 'http://localhost:8000', // Make sure this matches your Python backend's port
