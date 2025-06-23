fd8bfe22ec0d320e86c184ba5e112e06533d3e19
samlyme <sam@samly.me>
2025-06-22 22:35:34 -0700

    remove CORS for proxy

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,21 +9,6 @@ from app.routers import auth
 
 app = FastAPI()
 
-# TODO: fix this 
-origins = [
-    "http://localhost:5173",
-    "http://localhost:5174",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
 app.include_router(users.router)
 app.include_router(auth.router)
 
