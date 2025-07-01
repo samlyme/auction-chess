ca2210782589f7800ec204dd63a7346eca6970c5
samlyme <sam@samly.me>
2025-07-01 02:27:26 -0700

    update todo

--- backend/app/routers/users.py
+++ backend/app/routers/users.py
@@ -26,7 +26,8 @@ def add_user(db: DBDep, user_in: UserCredentials) -> UserProfile:
 @router.get("/")
 def get_user_by_username(db: DBDep, username: str | None = None, uuid: UUID | None = None) -> UserProfile | list[UserProfile]:
     # NOTE: If a uuid is provided, the username is ignored.
-    if (not username) and (not uuid): return get_users(db)
+    if (not username) and (not uuid): 
+        return get_users(db)
 
     stmt = select(User).where(User.uuid == uuid) if uuid else select(User).where(User.username == username)
     user = db.scalar(stmt)
@@ -39,7 +40,6 @@ def get_user_by_username(db: DBDep, username: str | None = None, uuid: UUID | No
 def get_users(db: DBDep) -> list[UserProfile]:
     return [user.profile() for user in db.scalars(select(User).limit(50))]
 
-# TODO: Proper "get me" route
 @router.get("/me")
 def get_current_user(current_user: CurrentUserDep) -> UserProfile:
     return current_user
