8c0a19fa42fd93bdd6bd42f8eb587cb6125acf68
samlyme <sam@samly.me>
2025-07-01 02:29:22 -0700

    clean imports

--- backend/app/core/auction_chess/game.py
+++ backend/app/core/auction_chess/game.py
@@ -1,11 +1,7 @@
 from abc import ABC, abstractmethod
-from uuid import uuid1
 
-from app.core.auction_chess.board import Board, Color, GamePhase, Marker, Move, Position
-from app.core.auction_chess.rules import effects, en_passant_test_board_factory, pawn_double_move_effect, set_has_moved_effect
+from app.core.auction_chess.board import Marker, Position
 
-# All the types defined here are for the interface between BE and FE
-from app.core.utils import PriorityQueue
 import app.schemas.types as api
 
 class Game(ABC):
