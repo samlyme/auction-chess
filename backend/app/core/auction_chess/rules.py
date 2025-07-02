01bc1cd0b869d844481b05dc5f1098ba348b2796
samlyme <sam@samly.me>
2025-07-02 01:34:25 -0700

    update rules

--- backend/app/core/auction_chess/rules.py
+++ backend/app/core/auction_chess/rules.py
@@ -1,35 +1,43 @@
-from app.core.auction_chess.board import (
-    BoardState,
-    Color,
-    Effect,
-    Marker,
-    MarkerTarget,
-    Piece,
-    PieceType,
-    Square,
-)
+from app.core.auction_chess.pieces import Bishop, King, Knight, Pawn, Queen, Rook
+from app.core.auction_chess.types import BoardState, Effect, Marker, MarkerTarget, Piece, Position, Square
+from app.schemas.types import Color, PieceType
 
+def piece_factory(type: PieceType, color: Color, position: Position) -> Piece:
+    if type == "p":
+        return Pawn(color, position)
+    elif type == "r":
+        return Rook(color, position)
+    elif type == "n":
+        return Knight(color, position)
+    elif type == "b":
+        return Bishop(color, position)
+    elif type == "q":
+        return Queen(color, position)
+    elif type == "k":
+        return King(color, position)
+    else:
+        raise Exception("Piece not defined")
 
 def standard_board_factory() -> BoardState:
     board: BoardState = [[Square() for _ in range(8)] for _ in range(8)]
 
     piece_order: list[PieceType] = ["r", "n", "b", "q", "k", "b", "n", "r"]
     for index, piece in enumerate(piece_order):
-        board[0][index].piece = Piece(type=piece, color="w", hasMoved=False)
-        board[7][index].piece = Piece(type=piece, color="b", hasMoved=False)
+        board[0][index].piece = piece_factory(piece, "w", (0, index))
+        board[7][index].piece = piece_factory(piece, "b", (7, index))
 
     for i in range(8):
-        board[1][i].piece = Piece(type="p", color="w", hasMoved=False)
+        board[1][i].piece = piece_factory("p", "w", (1, i))
 
     for i in range(8):
-        board[6][i].piece = Piece(type="p", color="b", hasMoved=False)
+        board[6][i].piece = piece_factory("p", "b", (6, i))
 
     return board
 
 
 def any_opposite_pawn(color: Color) -> MarkerTarget:
     def f(piece: Piece):
-        return piece.type == "p" and piece.color != color
+        return isinstance(piece, Pawn) and piece.color != color
 
     return f
 
@@ -68,7 +76,7 @@ def effects(*effects: Effect) -> Effect:
 
 def en_passant_test_board_factory() -> BoardState:
     board: BoardState = [[Square() for _ in range(8)] for _ in range(8)]
-    board[1][0].piece = Piece(type="p", color="w", hasMoved=False)
+    board[1][0].piece = piece_factory("p", "w", (1,0))
 
-    board[3][1].piece = Piece(type="p", color="b", hasMoved=False)
+    board[3][1].piece = piece_factory("p", "b", (3,1))
     return board
