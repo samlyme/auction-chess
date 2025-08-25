a4e744715747a4037804f10be8ceaa2a54a79c37
samlyme <sam@samly.me>
2025-08-24 21:21:48 -0700

    add note

--- backend/app/routers/websocket.py
+++ backend/app/routers/websocket.py
@@ -8,6 +8,10 @@ router = APIRouter(prefix="/ws")
 
 @router.websocket("")
 async def ws(websocket: WebSocket, user: CurrentUserDep):
+    """
+    WebSockets will only be used to send info to the clients.
+    The client will never send info to the server via the WebSocket.
+    """
     try:
         while True:
             data = await websocket.receive_text()
