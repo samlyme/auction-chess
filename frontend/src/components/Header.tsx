140abda74166a24bd23f4caf35525886e4c3c72e
samlyme <sam@samly.me>
2025-08-31 13:24:15 -0700

    update header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,10 +1,13 @@
+import { useNavigate } from "react-router";
 import { useAuthContext } from "../contexts/Auth";
 
 function Header() {
     const { logout } = useAuthContext();
+    const navigate = useNavigate()
     return (
         <div className="frame">
             <button onClick={logout}>Logout</button>
+            <button onClick={() => navigate("/")}>Home</button>
         </div>
     )
 }
