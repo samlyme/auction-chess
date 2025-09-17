3ed40c83a30e90b444021a057b5185c2ae62c7a2
samlyme <sam@samly.me>
2025-09-17 10:38:59 -0700

    update types

--- backend/app/schemas/types.py
+++ backend/app/schemas/types.py
@@ -2,54 +2,9 @@ from datetime import datetime
 from typing import Annotated, Literal, Union
 from uuid import UUID
 from pydantic import BaseModel, Field
+import pydantic
 
-# For the love of god please keep types here
-
-Color = Literal["w", "b"]
-PieceType = Literal["p", "r", "n", "b", "q", "k"]
-GamePhase = Literal["bid", "move"]
-GameOutcome = Literal["pending", "draw"] | Color
-
-
-class Player(BaseModel):
-    color: Color
-    uuid: UUID
-
-
-class BoardPosition(BaseModel):
-    row: int = Field(..., ge=0, le=7)  # 0-7
-    col: int = Field(..., ge=0, le=7)  # 0-7
-
-
-class Move(BaseModel):
-    start: BoardPosition
-    end: BoardPosition
-
-    # TODO: Fix the API to not have to do this jank
-    def uci(self) -> str:
-        files = "ABCDEFGH"
-        return (
-            files[self.start.col]
-            + str(self.start.row + 1)
-            + files[self.end.col]
-            + str(self.end.row + 1)
-        )
-
-
-class Bid(BaseModel):
-    amount: int
-
-
-class Piece(BaseModel):
-    type: PieceType
-    color: Color
-    has_moved: bool = False  # Useful for castling, initial pawn moves
-
-
-BoardPieces = list[list[Piece | None]]
-
-LegalMoves = list[list[list[BoardPosition]]]
-
+from main import AuctionStyle, Bid as GameBid
 
 # Users and such
 class UserCredentials(BaseModel):
@@ -85,6 +40,52 @@ class TokenResponse(BaseModel):
     access_token: str
     token_type: str
 
+# For the love of god please keep types here
+
+Color = Literal["w", "b"]
+PieceType = Literal["p", "r", "n", "b", "q", "k"]
+GamePhase = Literal["bid", "move"]
+GameOutcome = Literal["pending", "draw"] | Color
+
+
+class Player(BaseModel):
+    color: Color
+    uuid: UUID
+
+
+class BoardPosition(BaseModel):
+    row: int = Field(..., ge=0, le=7)  # 0-7
+    col: int = Field(..., ge=0, le=7)  # 0-7
+
+
+class Move(BaseModel):
+    start: BoardPosition
+    end: BoardPosition
+
+    # TODO: Fix the API to not have to do this jank
+    def uci(self) -> str:
+        files = "ABCDEFGH"
+        return (
+            files[self.start.col]
+            + str(self.start.row + 1)
+            + files[self.end.col]
+            + str(self.end.row + 1)
+        )
+
+
+@pydantic.dataclasses.dataclass(frozen=True)
+class Bid(GameBid):
+    pass
+
+class Piece(BaseModel):
+    type: PieceType
+    color: Color
+    has_moved: bool = False  # Useful for castling, initial pawn moves
+
+
+BoardPieces = list[list[Piece | None]]
+
+LegalMoves = list[list[list[BoardPosition]]]
 
 LobbyIdLength = 5
 LobbyId = str
@@ -108,23 +109,33 @@ class LobbyPacket(BaseModel):
 
 Players = dict[Color, UUID]
 Balances = dict[Color, int]
+OpenBidHistory = list[list[Bid]]
+class OpenFirst:
+    auction_style: AuctionStyle = "open_first"
+    bid_history: OpenBidHistory
+    
+    
+AuctionData = Annotated[Union[OpenFirst], Field(discriminator="auction_style")]
 
-
-class GamePacket(BaseModel):
-    type: PacketType = "game_packet"
-
+class GameData(BaseModel):
     outcome: GameOutcome
 
     phase: GamePhase
+    bid_turn: Color
     turn: Color
-
-    prev_bid: int
 
     board: BoardPieces
     moves: LegalMoves
 
     players: Players
     balances: Balances
+
+    auction_data: AuctionData
+
+class GamePacket(BaseModel):
+    type: PacketType = "game_packet"
+
+    content: GameData
 
 
 Packet = Annotated[Union[LobbyPacket, GamePacket], Field(discriminator="type")]
