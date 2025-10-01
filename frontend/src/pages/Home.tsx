f81f7f3422a6668d90c8d20c4bf62bd6a6176ee9
samlyme <sam@samly.me>
2025-09-30 17:39:05 -0700

    add description to home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,22 +1,31 @@
-import { Link, useNavigate } from 'react-router';
-import { useEffect } from 'react';
+import { Navigate } from 'react-router';
 import useAuth from '../hooks/useAuth';
 
 function Home() {
   const { token } = useAuth();
-  const navigate = useNavigate();
 
-  useEffect(() => {
-    if (token) navigate('/lobbies');
-  }, [navigate, token]);
+  if (token) {
+    return (
+      <Navigate to="/lobbies" />
+    )
+  }
 
   return (
     <div className="home">
       <h1>Welcome to Auction Chess</h1>
-      <h2>A novel chess variant with pricing, bluffing, and illegal moves.</h2>
-      <Link to="/auth">
-        <h1>login/signup</h1>
-      </Link>
+      <h4>A novel chess variant with pricing, bluffing, and illegal moves.</h4>
+      <button>Play now!</button>
+      <button>Learn more.</button>
+
+      <h2>What is auction chess?</h2>
+
+      <p>
+        Auction chess is a novel chess variant that incorporates a pricing and 
+        resource management system into traditional chess. Strategic pricing 
+        and management are required to win!
+      </p>
+
+      <button>Rules</button>
     </div>
   );
 }
