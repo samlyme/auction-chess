012fe7dd46f03f84517a0bebf7de29f9b20eb5a0
samlyme <sam@samly.me>
2025-10-31 03:18:04 -0700

    working move validation frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,86 @@
 import { Chessboard, type PieceDropHandlerArgs } from "react-chessboard";
-import { Chess, parseSquare, SquareSet, type NormalMove, type Square } from "chessops";
+import {
+  Chess,
+  parseSquare,
+  SquareSet,
+  type NormalMove,
+  type Square,
+} from "chessops";
 import "./index.css";
 import { useRef, useState } from "react";
 import { makeFen } from "chessops/fen";
 
+function* validMovesGenerator(
+  allDests: Map<Square, SquareSet>
+): Generator<NormalMove> {
+  for (const [start, ends] of allDests.entries()) {
+    for (const end of ends) {
+      yield { from: start, to: end };
+    }
+  }
+}
 
 export function App() {
-
   const chessGameRef = useRef(Chess.default());
   const chessGame = chessGameRef.current;
 
-  const [chessPosition, setChessPosition] = useState(makeFen(chessGame.toSetup()));
+  const [chessPosition, setChessPosition] = useState(
+    makeFen(chessGame.toSetup())
+  );
 
   function makeRandomMove() {
-    const possibleMoves = chessGame.allDests();
+    const validMoves = validMovesGenerator(chessGame.allDests());
 
-    function* validMovesGenerator(): Generator<NormalMove> {
-      for (const [start, ends] of possibleMoves.entries()) {
-        for (const end of ends) {
-          yield { from: start, to: end};
-        }
-      }
-    }
-
-    const validMoves = validMovesGenerator();
-    
     let move: NormalMove | null = null;
-    let count = 0
+    let count = 0;
     for (const newMove of validMoves) {
       count += 1;
       if (Math.random() * count < 1) {
-        move = newMove
+        move = newMove;
       }
     }
-    
-    chessGame.play(move!)
-    setChessPosition(makeFen(chessGame.toSetup()))
+
+    chessGame.play(move!);
+    setChessPosition(makeFen(chessGame.toSetup()));
   }
 
-  function onPieceDrop({sourceSquare, targetSquare}: PieceDropHandlerArgs) {
+  function onPieceDrop({ sourceSquare, targetSquare }: PieceDropHandlerArgs) {
     if (!targetSquare) return false;
 
-    try {
-      chessGame.play({
-        from: parseSquare(sourceSquare)!,
-        to: parseSquare(targetSquare)!,
-        // promotion: "queen"
-      });
+    const move: NormalMove = {
+      from: parseSquare(sourceSquare)!,
+      to: parseSquare(targetSquare)!,
+      // promotion: "queen"
+    };
 
-      setChessPosition(makeFen(chessGame.toSetup()))
-      makeRandomMove()
-      return true;
-    }
-    catch {
+    const validMoves = validMovesGenerator(chessGame.allDests());
+
+    if (
+      !validMoves.some(
+        (value: NormalMove) => value.from === move.from && value.to === move.to
+      )
+    ) {
       return false;
     }
+
+    chessGame.play(move);
+
+    setChessPosition(makeFen(chessGame.toSetup()));
+
+    makeRandomMove();
+    return true;
   }
 
   const chessboardOptions = {
     position: chessPosition,
     onPieceDrop,
-    id: 'play-vs-random'
+    id: "play-vs-random",
   };
 
   return (
     <div className="app">
-      <div className="game" style={{width: "50%", margin: "auto"}}>
-        <Chessboard options={chessboardOptions}/>
+      <div className="game" style={{ width: "50%", margin: "auto" }}>
+        <Chessboard options={chessboardOptions} />
       </div>
     </div>
   );
