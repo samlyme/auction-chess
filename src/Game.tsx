5a9b4e421d0c693a62186fe7153a71a291ff625c
samlyme <sam@samly.me>
2025-11-04 10:23:49 -0800

    working click to move

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,9 +1,13 @@
 import type { Game, Move, MoveMap } from "boardgame.io";
 import { PseudoChess } from "./pseudoChess";
 import { makeBoardFen, makeFen, parseFen } from "chessops/fen";
-import { parseSquare, type NormalMove, type Setup } from "chessops";
+import { makeSquare, parseSquare, type NormalMove, type Setup } from "chessops";
 import { INVALID_MOVE } from "boardgame.io/core";
-import { Chessboard, type SquareHandlerArgs } from "react-chessboard";
+import {
+  Chessboard,
+  type PieceDropHandlerArgs,
+  type SquareHandlerArgs,
+} from "react-chessboard";
 import type { BoardProps } from "boardgame.io/dist/types/packages/react";
 import { useState } from "react";
 
@@ -15,13 +19,9 @@ export interface ChessState {
   fen: string;
 }
 
-
 const movePiece: Move<ChessState> = ({ G }, move: NormalMove) => {
+  // Consider a refactor. Recreating the class per move COULD be a bottleneck.
   const chessLogic = new PseudoChess(G.fen);
-
-  // TODO: Implement pseudomove isLegal
-  // if (!chessLogic.isLegal(move)) return INVALID_MOVE;
-
   chessLogic.movePiece(move);
   G.fen = chessLogic.toFen();
 };
@@ -40,42 +40,82 @@ export const PseudoChessGame: Game<ChessState> = {
     movePiece: movePiece,
   },
 
-  endIf: ({G, ctx}) => {
+  endIf: ({ G, ctx }) => {
     const chessLogic: PseudoChess = new PseudoChess(G.fen);
-    
+
     const outcome = chessLogic.outcome();
     if (outcome.winner) return outcome;
-  }
+  },
 };
 
 export function PseudoChessBoard({ G, moves }: BoardProps) {
-    const [moveFrom, setMoveFrom] = useState("");
+  const [moveFrom, setMoveFrom] = useState<string | null>(null);
 
-  function onSquareClick({ square, piece }: SquareHandlerArgs) {
-    if (moveFrom == "" && piece) {
-      setMoveFrom(square)
-    }
-    else if (square == moveFrom) setMoveFrom("");
-    else {
-      const move: NormalMove = {
-        from: parseSquare(moveFrom)!,
-        to: parseSquare(square)!
+  const chessLogic = new PseudoChess(G.fen);
+
+  const moveOptions = moveFrom
+    ? chessLogic.legalDests(parseSquare(moveFrom)!)
+    : [];
+  const squareStyles: Record<string, React.CSSProperties> = {};
+  if (moveFrom) {
+    squareStyles[moveFrom] = {
+      background: "rgba(255, 255, 0, 0.4)",
+    };
+  }
+  for (const moveOption of moveOptions) {
+    squareStyles[makeSquare(moveOption)] = chessLogic.get(moveOption)
+      ? {
+          background:
+            "radial-gradient(circle, rgba(0,0,0,.2) 85%, transparent 85%)",
+          borderRadius: "50%",
+        }
+      : {
+          background:
+            "radial-gradient(circle, rgba(0,0,0,.2) 25%, transparent 25%)",
+          borderRadius: "50%",
+        };
+  }
+
+  const onPieceDrop = ({
+    sourceSquare,
+    targetSquare,
+  }: PieceDropHandlerArgs): boolean => {
+    if (!targetSquare) return false;
+
+    moves.movePiece!({
+      from: parseSquare(sourceSquare),
+      to: parseSquare(targetSquare),
+    });
+    return true;
+  };
+
+  function onSquareClick({ square, piece }: SquareHandlerArgs): void {
+    console.log("moveFrom", moveFrom);
+
+    if (moveFrom !== null) {
+      if (square !== moveFrom) {
+        moves.movePiece!({
+          from: parseSquare(moveFrom),
+          to: parseSquare(square),
+        });
       }
-      console.log("attemping click move", {moveFrom, square, move});
-      setMoveFrom("")
-      moves.movePiece!(move);
+      setMoveFrom(null);
+    } else if (piece !== null) {
+      setMoveFrom(square);
+    } else {
+      setMoveFrom(null);
     }
   }
 
   return (
     // TODO: fix animation
-    <Chessboard options={{
-      position: G.fen,
-      onPieceDrop: ({sourceSquare, targetSquare}): boolean => {
-        moves.movePiece!({from: parseSquare(sourceSquare), to: parseSquare(targetSquare!)})
-        return true
-      },
-      onSquareClick,
-    }} />
-  )
+    <Chessboard
+      options={{
+        position: G.fen,
+        onPieceDrop,
+        onSquareClick,
+        squareStyles,
+      }}
+    />
+  );
 }
