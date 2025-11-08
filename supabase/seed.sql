17543ac994845dd0599b27ee5eb21599257911f2
samlyme <sam@samly.me>
2025-11-08 01:08:48 -0800

    supabase init

---
+++ supabase/seed.sql
@@ -0,0 +1,20 @@
+insert into public.employees
+  (name, email)
+values
+  ('Erlich Bachman', 'ebach@sup.co'),
+  ('Richard Hendricks', 'rhend@sup.co'),
+  ('Monica Hall', 'mhall@sup.co');
+
+-- Insert dummy seed data into the lobbies table
+INSERT INTO public.lobbies (code)
+VALUES
+  ('ABCD12'),
+  ('QWER34'),
+  ('ZXCV56'),
+  ('TYUI78'),
+  ('MNOP90'),
+  ('GAME01'),
+  ('ROOM22'),
+  ('JOIN33'),
+  ('PLAY44'),
+  ('TEST55');
