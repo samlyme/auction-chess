8a08aba1c3bcfcfc9fd3b54fc800523bcecac82b
samlyme <sam@samly.me>
2025-11-07 17:23:43 -0800

    scuffed bgio implementation.

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,23 @@ import { AuctionChessBoard } from "./components/Game";
 import { Client } from "boardgame.io/react";
 import "./styles/App.css";
 import { AuctionChessGame } from "./game/auctionChess";
-import { Local } from "boardgame.io/multiplayer";
+import { Local, SocketIO } from "boardgame.io/multiplayer";
 
 const ClientA = Client({
   game: AuctionChessGame,
   board: AuctionChessBoard,
-  multiplayer: Local(),
+  multiplayer: SocketIO({ server: 'localhost:3001' }),
   numPlayers: 2,
 });
 
+const color = prompt("white or black");
+
 export function App() {
+  
   return (
     <>
       <div className="app">
-        <ClientA playerID="white" />
-      </div>
-      <div className="app">
-        <ClientA playerID="black" />
+        <ClientA playerID={color!} />
       </div>
     </>
   );
