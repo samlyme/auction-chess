b47e5ab4d134ee875eed64c55b6ad7a04cbf0994
samlyme <sam@samly.me>
2025-11-22 02:52:11 -0800

    auth stages fragile

--- clients/web/src/pages/CreateUserProfile.tsx
+++ clients/web/src/pages/CreateUserProfile.tsx
@@ -6,6 +6,7 @@ import { AuthContext } from "../contexts/Auth";
 export default function CreateUserProfile() {
   // assume session is good
   const {session} = useContext(AuthContext)
+  console.log("in create user profile", {session});
   const [newProfile, setNewProfile] = useState<Omit<Tables<'profiles'>, 'created_at'>>({username: "", bio: "", id: session!.user.id});
 
   async function submitTask(_e: FormEvent) {
@@ -13,15 +14,20 @@ export default function CreateUserProfile() {
     await supabase.from('profiles').insert(newProfile)
   }
 
+  
+
   return (
-    <form onSubmit={submitTask}>
-      <input type="text" placeholder='Username' value={newProfile.username} onChange={e => setNewProfile(
-        prev => ({...prev, username: e.target.value})
-      )} />
-      <textarea placeholder='Task Description' value={newProfile.bio || ""} onChange={e => setNewProfile(
-        prev => ({ ...prev, bio: e.target.value})
-      )}></textarea>
-      <button type="submit">Submit</button>
-    </form>
+    <>
+      <h1>create profile</h1>
+      <form onSubmit={submitTask}>
+        <input type="text" placeholder='Username' value={newProfile.username} onChange={e => setNewProfile(
+          prev => ({...prev, username: e.target.value})
+        )} />
+        <textarea placeholder='Task Description' value={newProfile.bio || ""} onChange={e => setNewProfile(
+          prev => ({ ...prev, bio: e.target.value})
+        )}></textarea>
+        <button type="submit">Submit</button>
+      </form>
+    </>
   )
 }
