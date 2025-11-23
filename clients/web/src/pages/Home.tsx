62ac9fd445978f12d2042c0224c50aeb4c2c480a
samlyme <sam@samly.me>
2025-11-23 00:32:55 -0800

    more lobbies crud

--- clients/web/src/pages/Home.tsx
+++ clients/web/src/pages/Home.tsx
@@ -1,7 +1,9 @@
 import { Link } from "react-router";
 import supabase from "../supabase";
+import { useState } from "react";
 
 export default function Home(){
+    const [code, setCode] = useState<string>("")
     return <>
         <h1>Home</h1>
 
@@ -12,6 +14,8 @@ export default function Home(){
         </h2>
 
         <h2>Make lobby</h2>
+        
+        {/* This is such a thin wrapper, I might as well just use fetch lol */}
         <button onClick={() => supabase.functions.invoke("api/lobbies", {
             method: "POST"
         }).then((val) => {
@@ -20,6 +24,19 @@ export default function Home(){
         })}>make lobby</button>
 
         <h2>Join Lobby</h2>
+        <input type="text" value={code} onChange={(e) => {
+            setCode(e.target.value);
+        }}></input>
+        <button onClick={() => {
+            supabase.functions.invoke(`api/lobbies/${code}/join`, {
+                method: "POST",
+            })
+            .then(val => {
+                console.log(val);
+            })
+        }}>join</button>
+
+
         <button onClick={() => supabase.auth.signOut()}>sign out</button>
     </>
 }
