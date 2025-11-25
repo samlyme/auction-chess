409d4f40d68c315b9de0c8ff95aabf56e67f5871
samlyme <sam@samly.me>
2025-11-24 21:31:19 -0800

    shared package!

--- supabase/functions/api/routes/lobbies.ts
+++ supabase/functions/api/routes/lobbies.ts
@@ -1,8 +1,11 @@
+// deno-lint-ignore-file no-explicit-any
 import { Context, Hono, MiddlewareHandler } from "hono";
 import { Tables } from "../../_shared/database.types.ts";
 import { supabase } from "../supabase.ts";
-import { CompleteEnv, LobbyEnv } from "../types.ts";
 import { generateCode } from "../utils.ts";
+import { LobbyEnv } from "../types.ts";
+
+import { UserProfile } from "shared";
 
 // Inserts a row with a unique code into "lobbies"
 export async function createLobbyRow(
@@ -53,13 +56,13 @@ const lobbyValidator: MiddlewareHandler<LobbyEnv> = async (
   await next();
 };
 
-const app = new Hono();
+const app = new Hono<LobbyEnv>();
 
 app.use(lobbyValidator);
 
 // Need lobbies middleware. Ignore weird states for now.
 app.post("", async (c: Context<LobbyEnv>) => {
-  if(c.get("lobby")) return c.json({ message: "user already in lobby" }, 400)
+  if (c.get("lobby")) return c.json({ message: "user already in lobby" }, 400);
 
   const lobby = await createLobbyRow({}, c.get("user").id);
 
@@ -69,14 +72,36 @@ app.post("", async (c: Context<LobbyEnv>) => {
   return c.json(lobby);
 });
 
-app.get("", async (c: Context<LobbyEnv>) => {
+app.get("", (c: Context<LobbyEnv>) => {
   return c.json(c.get("lobby"));
 });
 
-app.post("/:code/join", async (c: Context<LobbyEnv>) => {
-  if(c.get("lobby")) return c.json({ message: "user already in lobby" }, 400)
+app.delete("/:code", async (c: Context<LobbyEnv>) => {
+  const lobby = c.get("lobby");
+  if (!lobby) return c.status(400);
 
   const code = c.req.param("code");
+  if (lobby.code !== code) return c.status(400);
+
+  const user = c.get("user");
+  if (user.id !== lobby.host_uid) return c.status(400);
+
+  const { data, error } = await supabase
+    .from("lobbies")
+    .delete()
+    .eq("code", code)
+    .select()
+    .single();
+  if (error) return c.json(error, 500);
+
+  return c.json(data);
+});
+
+app.post("/:code/join", async (c: Context<LobbyEnv>) => {
+  if (c.get("lobby")) return c.json({ message: "user already in lobby" }, 400);
+
+  const code = c.req.param("code");
+  // TODO: check if lobby is full
   const { data: lobby } = await supabase
     .from("lobbies")
     .update({ guest_uid: c.get("user").id })
@@ -85,6 +110,27 @@ app.post("/:code/join", async (c: Context<LobbyEnv>) => {
     .maybeSingle();
 
   return c.json(lobby);
+});
+
+app.post("/:code/leave", async (c: Context<LobbyEnv>) => {
+  const lobby = c.get("lobby");
+  if (!lobby) return c.json({ message: "user not in lobby" }, 400);
+
+  // TODO: make remove code from param
+  const code = c.req.param("code");
+  if (code !== lobby.code) return c.json({ message: `user in other lobby, not in lobby ${code}`});
+
+  const user = c.get("user");
+  if (user.id !== lobby.guest_uid) return c.json({ message: `user is not guest in lobby ${code}`});
+
+  const { data, error } = await supabase
+    .from("lobbies")
+    .update({ guest_uid: null })
+    .eq("code", code)
+    .select()
+    .maybeSingle();
+
+    return c.json(data);
 });
 
 export { app as lobbies };
