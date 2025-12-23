29501ac6e5f85388912f218dada4df51807fbd52
samlyme <sam@samly.me>
2025-12-22 20:54:19 -0800

    use host frontend on cloudflare

--- clients/web/vite.config.ts
+++ clients/web/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { cloudflare } from "@cloudflare/vite-plugin"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), cloudflare()],
   server: {
     port: 3000,
   },
