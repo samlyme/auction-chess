e89e90de4cd4b77092e904b77014904bf6a28258
samlyme <sam@samly.me>
2025-12-24 22:21:13 -0800

    move supabase def into index

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,13 @@
 import { createApp } from "./app";
-import { supabase } from "./supabase";
+import { createClient } from "@supabase/supabase-js";
+import process from "node:process";
+
+export const supabase = createClient(
+  process.env["SUPABASE_URL"]!,
+  // supabase service role key works here
+  // secret key doesn't.
+  process.env["SUPABASE_SERVICE_ROLE_KEY"]!,
+);
 
 const app = createApp(supabase);
 
